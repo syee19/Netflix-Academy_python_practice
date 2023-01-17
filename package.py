4bd465d7916d3229e7db81ba16cca251556ecc61
Youngbin Park <isakeisv@gmail.com>
2023-01-17 13:38:02 +0900

    REZ package

---
+++ package.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+name = 'sinyulee'
+version = '1.0.4'
+authors = ['Sinyu Lee']
+requires = [
+    'python',
+]
+variants = [
+    ['platform-linux', 'python'],
+]
+tools = ['todo']
+
+def commands():
+    env.PATH.append("{root}/bin")
+    env.PYTHONPATH.prepend("{root}/python")
+
+
+format_version = 2
